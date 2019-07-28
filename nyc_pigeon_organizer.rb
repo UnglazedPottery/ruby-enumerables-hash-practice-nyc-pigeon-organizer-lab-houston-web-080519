8cb06d441061bcedeca01a8966d2f8f35743b278
Jade Huynh <35jadeh@gmail.com>
2019-07-28 01:46:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,41 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeon_names = []
+  data[:gender].keys.each do |pigeon|
+    pigeon_names << pigeon
+  end
+  
+  pigeon_names.each do |bird|
+    pigeon_list[bird] = {
+      color: [],
+      gender: [],
+      lives: []
+    }
+  end
+  
+  data[:color].each do |color, name_array|
+    name_array.each do |name|
+      if data[:color][color].include?(name)
+        pigeon_list[name][:color] << color.to_s
+      end
+    end
+  end
+  
+  data[:gender].each do |gender, name_array|
+    name_array.each do |name|
+      if data[:gender][gender].include?(name)
+        pigeon_list[name][:gender] << gender.to_s
+      end
+    end
+  end
+  
+  data[:lives].each do |place, name_array|
+    name_array.each do |name|
+      if data[:lives][place].include?(name)
+        pigeon_list[name][:lives] << place.to_s
+      end
+    end
+  end
+  
+  pigeon_list
 end
